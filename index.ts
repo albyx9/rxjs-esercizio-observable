3e9403036d9ca9393abed25183f693e84ed06a6a
albyx9 <alessandrolabate96@hotmail.it>
2021-04-23 15:35:51 +0200

    2 files modified, 1 added

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-//ESERCIZIO: arresta la temperatura dopo il quarto valore
 import { Observable, interval, timer } from "rxjs";
 
 const apiKey = "d11be8fa8826b6777bc94891b01f0767";
@@ -6,35 +5,36 @@ const URL =
   "https://api.openweathermap.org/data/2.5/weather?APPID=" +
   apiKey +
   "&units=metric&q=";
-var city = document.getElementById("citta").innerHTML;
-var separatore = " -------------------------- ";
-//è un observable produce una serie di numeri periodicamenti 
-const tick = interval(1000000);
-//Costruisco l'observable, una classe di oggetti
-let conta = 0;
-const temp = new Observable(subscriber => tick.subscribe( { 
-//publisher produce dati
-//i dati vengono consumati con il metodo subscribe
+  const tick = interval(1000);
+  
 
-  next(n) { 
-    fetch(URL + city)
-      .then(response =>  response.json())
-        .then(data => 
-          subscriber.next(data.main.temp))        
-  }
-   } ) ); 
+//il metodo utilizzato restituisce un Array oggetto da un oggetto con proprietà iterable
+var cityElems = Array.from(document.getElementsByClassName("citta"));
 
-// Due subscriber con un solo parametro, quello obblihatorio
-//i subscriber accedono ai dati prodotti dal publisher
-temp.subscribe({   
-  next(x) {console.log(x);}
-})
-temp.subscribe({   
-  next(x) {
-    conta+=1;
-    document.getElementById("output").innerHTML+=
-  x + "<br>";},
-  complete() {
-    document.getElementById("fine").innerHTML = "Servizio temperatura: TERMINATO";
-  }
-})
+
+for (let elem of cityElems) {
+  elem.onclick = () => display(elem.innerHTML);
+}
+
+document.getElementById("calcoloMedia").onclick = () => media();
+
+function doCity(city, callback) {
+  let promise = fetch(URL + city)
+    .then(response => response.json(), error => alert(error))
+    .then(data => callback(data));
+  return promise;
+}
+async function display(city) {
+  let t = await doCity(city, data => data.main.temp);
+  document.getElementById("risposta").innerHTML =
+    "A " + city + " ci sono " + t + " gradi";
+}
+
+//async costruisce una funzione che restituisce una promise
+async function media() {
+  let temps = await Promise.all(
+    cityElems.map(cityElem => doCity(cityElem.innerHTML, data => data.main.temp))
+  );
+  let somma = temps.reduce((somma, temp) => temp + somma);
+  document.getElementById("media").innerText = somma / cityElems.length;
+}
